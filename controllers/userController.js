4e86420d96cfb1c77c013ed7cee65bb9f46226af
JatinBisht2308 <jatinbisht2308@gmail.com>
2022-09-09 22:39:23 +0530

    :fire: different crud operation added to the userRoute

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,3 +1,19 @@
 exports.signUp = async(req,res,next) => {
     return res.status(200).send({message:"hitting the sign up route"})
 }
+// Login route
+exports.logIn = async(req,res,next) => {
+    return res.status(200).send({message:"hitting the login route"})
+}
+// Update user route
+exports.updateUser = async(req,res,next) => {
+    return res.status(200).send({message:"hitting the update user route"})
+}
+// Delete User route
+exports.deleteUser = async(req,res,next) => {
+    return res.status(200).send({message:"hitting the delete user route"})
+}
+// Data route
+exports.data = async(req,res,next) => {
+    return res.status(200).send({message:"hitting the data route"}) 
+}
--- routes/userRoute.js
+++ routes/userRoute.js
@@ -3,5 +3,8 @@ const userController = require("../controllers/userController")
 const router = require('express').Router();
 
 router.post("/", userController.signUp);
-
+router.post("/login",userController.logIn);
+router.patch(`userId`,userController.updateUser);
+router.delete(`userId`,userController.deleteUser);
+router.get('/data',userController.data);
 module.exports = router;
