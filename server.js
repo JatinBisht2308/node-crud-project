2bf0f7de2edfd7226f751bf0c965e44ba85325e7
JatinBisht2308 <jatinbisht2308@gmail.com>
2022-09-09 22:09:46 +0530

    :fire: created the .env file for sources

--- server.js
+++ server.js
@@ -1,8 +1,12 @@
 const app = require("./app");
 const http = require("http");
 const server = http.createServer(app);
-
-const port = 3000;
+if(process.env.NODE_ENV !== "production")
+{
+    require("dotenv").config();
+}
+// if there is PORT in .env then we will use this else we use 3000 as port number
+const port = process.env.PORT || 3000;
 
 server.listen(port,()=>{
     console.log(`Server is running on http://localhost:${port}`);
